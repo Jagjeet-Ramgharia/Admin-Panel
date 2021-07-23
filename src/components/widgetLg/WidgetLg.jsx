8d85877ced9e103f2a643a13ef81ea8d3cabc5bc
Jagjeet-Ramgharia <75858518+Jagjeet-Ramgharia@users.noreply.github.com>
2021-07-23 16:08:43 +0530

    updated

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -2,9 +2,47 @@ import React from 'react';
 import './widgetlg.css'
 
 const WidgetLg = () => {
+    const Button = ({type}) =>{
+        return <button className={"widgetlg_btn" + type}>{type}</button>
+    };
     return (
         <div className="widget_lg">
-            large
+            <h3 className="title">Latest Transactions</h3>
+            <table className="widgetlg_table">
+                <tr className="row">
+                    <th className="th">Customers</th>
+                    <th className="th">Date</th>
+                    <th className="th">Amount</th>
+                    <th className="th">Status</th>
+                </tr>
+                <tr className="row">
+                    <td className="user">
+                        <img className="t_img" src="https://live.staticflickr.com/5252/5403292396_0804de9bcf_b.jpg" alt=""/>
+                        <span className="name">Susan carol</span>
+                    </td>
+                    <td className="date">23/7/2021</td>
+                    <td className="amount">$100</td>
+                    <td className="date"><Button type="Approved"/></td>
+                </tr>
+                <tr className="row">
+                    <td className="user">
+                        <img className="t_img" src="https://live.staticflickr.com/5252/5403292396_0804de9bcf_b.jpg" alt=""/>
+                        <span className="name">Susan carol</span>
+                    </td>
+                    <td className="date">23/7/2021</td>
+                    <td className="amount">$100</td>
+                    <td className="date"><Button type="Pending"/></td>
+                </tr>
+                <tr className="row">
+                    <td className="user">
+                        <img className="t_img" src="https://live.staticflickr.com/5252/5403292396_0804de9bcf_b.jpg" alt=""/>
+                        <span className="name">Susan carol</span>
+                    </td>
+                    <td className="date">23/7/2021</td>
+                    <td className="amount">$100</td>
+                    <td className="date"><Button type="Declined"/></td>
+                </tr>
+            </table>
         </div>
     )
 }
